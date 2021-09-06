a4113288dbd9214f34b1b94f05bff341f480907b
DurbeKK <k2.durbek@gmail.com>
2021-09-06 16:08:19 +0500

    Modified the loader file
    Added a variable that will point to the path of pdf files

--- loader.py
+++ loader.py
@@ -4,7 +4,9 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data import config
+from os import getcwd
 
+path = getcwd() + "/pdf_files"
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
